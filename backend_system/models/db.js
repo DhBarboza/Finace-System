06d77735fd078e43772fa42cc5827755d749c9be
Douglas Henrique <dev.dhbarboza@gmail.com>
2021-07-09 14:14:56 -0300

    update database settings

--- backend_system/models/db.js
+++ backend_system/models/db.js
@@ -1,2 +1,9 @@
+// Settings conection with MySql database:
 const { Sequelize } = require('sequelize');
 
+const sequelize = new Sequelize('finance', 'root', '', {
+  host: 'localhost',
+  dialect: 'mysql'
+});
+
+module.exports = sequelize;
